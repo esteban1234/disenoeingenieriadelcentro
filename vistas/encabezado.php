4deecc4a063c836308bfddd4239b878b2941e128
esteban1234 <tva_libra@hotmail.com>
2018-06-07 13:38:15 -0500

    metas

--- vistas/encabezado.php
+++ vistas/encabezado.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	
+	<meta name="google-site-verification" content="fZzEzpCe1LsrJ3P7i5qdOXE5qlkbuyfRZ2UwSN_qr88" />
 	<link rel="icon" type="image/png" href="img/favicon.png" />
 	<title>DISEÑO E INGENIERIA DEL CENTRO</title>
 	 <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"> 
